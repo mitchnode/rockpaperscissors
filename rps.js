91c357a469ce583f7f5bfa9b185e6f7c1de0cb7d
mitchnode <70460735+mitchnode@users.noreply.github.com>
2023-12-19 14:32:17 +1100

    Added playRound and game functions

--- rps.js
+++ rps.js
@@ -14,15 +14,67 @@ function getComputerChoice(){
 
 // Prompt the player for thier choice, make case insensitive and check choice.
 function getPlayerChoice(){
+    let choice = '';
     let text = prompt('Please enter your choice (Rock, Paper, Scissors): ');
-    let choice = text.charAt(0).toUpperCase().concat(text.slice(1,text.length).toLowerCase())
+    if (text !== null){
+        choice = text.charAt(0).toUpperCase().concat(text.slice(1,text.length).toLowerCase())
+    } else {
+        console.log('Game Cancelled!');
+        return false;
+    }
     if (choice === 'Rock' || choice === 'Paper' || choice === 'Scissors'){
         return choice;
     } else {
-        return 'Incorrect entry. Please choose either Rock, Paper or Scissors.';
+        console.log('Incorrect entry. Please choose either Rock, Paper or Scissors.');
+        return getPlayerChoice();
     }
-
 }
-// Test functions
-console.log(getComputerChoice());
-console.log(getPlayerChoice());
+
+// Play a round returning the result
+function playRound(playerSelection,computerSelection){
+    console.log('Computer chose ' + computerSelection);
+    console.log('You chose ' + playerSelection);
+    if (playerSelection === computerSelection){
+        return ['It\'s a Tie!', 'T'];
+    } else if ((playerSelection === 'Rock' && computerSelection == 'Scissors') || (playerSelection === 'Scissors' && computerSelection == 'Paper') || (playerSelection === 'Paper' && computerSelection == 'Rock')){
+        return ['You Win! ' + playerSelection + ' beats ' + computerSelection, 'P'];
+    } else if ((computerSelection === 'Rock' && playerSelection == 'Scissors') || (computerSelection === 'Scissors' && playerSelection == 'Paper') || (computerSelection === 'Paper' && playerSelection == 'Rock')){
+        return ['You Lose! ' + computerSelection + ' beats ' + playerSelection, 'C'];
+    } else {
+        return ['Something went wrong!', 'T'];
+    }
+}
+
+//Play the game, best of 5 replaying ties.
+function game(){
+    let computerSelection = '';
+    let playerSelection = '';
+    let playerWins = 0;
+    let computerWins = 0;
+    while (playerWins < 3 && computerWins < 3){
+        computerSelection = getComputerChoice();
+        playerSelection = getPlayerChoice();
+        if (playerSelection === false){
+            break;
+        }
+        result = playRound(playerSelection,computerSelection);
+        console.log(result[0]);
+        if (result[1] === 'T'){
+            console.log('Play again!');
+        }
+        else if (result[1] === 'P'){
+            playerWins += 1;
+        } else if (result[1] === 'C'){
+            computerWins += 1;
+        }
+        console.log('Score - Player: ' + playerWins + ' Computer: '+ computerWins);
+    }
+    console.log('Game Over!');
+    if (playerWins > computerWins){
+        console.log('You Win!');
+    } else if (playerWins < computerWins){
+        console.log('You Lose!');
+    }
+}
+
+game();
